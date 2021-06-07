5a046c6210588e1779a36d21d3bcd539fcb3a80a
Anatolii Charikov <pepsh6881@gmail.com>
2021-06-07 11:17:29 +0300

    add the new file
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

---
+++ simple_snake_game.c
@@ -0,0 +1,50 @@
+#include <ncurses.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+enum { symbol_target = '+' };
+enum { symbol_snake = '*' };
+enum { symbol_barrier_plus = 'x' };
+enum { symbol_blank = ' ' };
+enum { symbol_comma = ',' };
+enum { symbol_parenthesis_left = '(' };
+enum { symbol_parenthesis_right = ')' };
+enum { key_escape = 27 };
+enum { key_spacebar = ' ' };
+enum { max_snake_length = 5 };
+enum { max_target_amount = 4 };
+enum { max_round_num = 5 };
+enum { snake_speed = 120 };
+
+static const char greeting_message[] = "Simple Snake Game";
+
+#define TRUE 1
+#define FALSE 0
+
+struct coordinate {
+	int x,y;
+};
+
+struct coordinate_double_list {
+	struct coordinate coord;
+	struct coordinate_double_list *next, *prev;
+};
+
+struct coordinate_deque {
+	struct coordinate_double_list *first, *last;
+};
+
+struct game_level_settings {
+	int round_num;
+	int current_snake_length;
+	int snake_speed;
+	int max_barrier_amount;
+};
+
+static void
+clear_screen()
+{
+	clear();
+	refresh();
+}
