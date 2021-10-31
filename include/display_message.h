4509e50bc986957da8168fd2aca7b1e7133bdea4
Anatolii Charikov <pepsh6881@gmail.com>
2021-10-31 22:41:55 +0200

    add the print_round_number function to the display_message module
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/display_message.h
+++ include/display_message.h
@@ -1,6 +1,10 @@
 #ifndef DISPLAY_MESSAGE_H_SETNRY
 #define DISPLAY_MESSAGE_H_SETNRY
 
-void display_message_center(const char *string);
+void
+display_message_center(const char *string);
+
+void
+print_round_number(const int number);
 
 #endif
--- src/display_message.c
+++ src/display_message.c
@@ -6,7 +6,8 @@
 #include "string.h"
 #include "screen.h"
 
-void display_message_center(const char *string)
+void
+display_message_center(const char *string)
 {
 	int max_screen_value_y, max_screen_value_x;
 	int string_len;
@@ -20,6 +21,28 @@ void display_message_center(const char *string)
 	init_pair(4, COLOR_MAGENTA, COLOR_BLACK);
 	attrset(COLOR_PAIR(4));
 	addstr(string);
+	refresh();
+	sleep(2);
+	clear_screen();
+}
+
+void
+print_round_number(const int number)
+{
+	static const char *message;
+	int max_screen_value_y, max_screen_value_x;
+	int string_len;
+
+	message = "Round";
+	clear_screen();
+	getmaxyx(stdscr, max_screen_value_y, max_screen_value_x);
+	string_len = string_length(message);
+	move(max_screen_value_y / 2, \
+						(max_screen_value_x - string_len - 1) / 2);
+	start_color();
+	init_pair(4, COLOR_MAGENTA, COLOR_BLACK);
+	attrset(COLOR_PAIR(4));
+	printw("%s %d", message, number);
 	refresh();
 	sleep(2);
 	clear_screen();
