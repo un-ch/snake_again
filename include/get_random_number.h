649d8a3a02eb4d2ff095ae0ce6beefc6c82a6751
Anatolii Charikov <pepsh6881@gmail.com>
2021-11-01 21:12:38 +0200

    add parameter to the get_random_int_number() function
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/get_random_number.h
+++ include/get_random_number.h
@@ -2,6 +2,6 @@
 #define GET_RANDOM_NUMBER_H_SENTRY
 
 int
-get_random_int_number();
+get_random_int_number(const int max_generated_number);
 
 #endif
--- src/get_random_number.c
+++ src/get_random_number.c
@@ -4,10 +4,12 @@
 #include "get_random_number.h"
 
 int
-get_random_int_number()
+get_random_int_number(const int max_generated_number)
 {
+	/*
 	const float max_generated_number = 15.0;
-
-	return (1 + (int)(max_generated_number * rand() / \
-											(RAND_MAX + 1.0)));
+	return (1 + (int)(max_generated_number* rand() / \
+										(RAND_MAX + 1.0)));
+	*/
+	return (rand() % (max_generated_number + 1));
 }
