aeddd25861fc225b3e9b19d417ac65c87f9d481a
Anatolii Charikov <pepsh6881@gmail.com>
2021-11-01 21:14:33 +0200

    remove the struct coordinate declaration
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/objects.h
+++ include/objects.h
@@ -1,15 +1,12 @@
 #ifndef OBJECTS_H_SENTRY
 #define OBJECTS_H_SENTRY
 
+#include "coordinate.h"
+
 enum { symbol_target = '+' };
 enum { symbol_snake = '*' };
 enum { symbol_barrier_plus = 'x' };
 enum { symbol_blank = ' ' };
-
-struct coordinate {
-	int x;
-	int y;
-};
 
 void 
 show_object_target(const struct coordinate coord);
