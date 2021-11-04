3131add7598d6f6fee85cce0d092ce2419a0c19f
Anatolii Charikov <pepsh6881@gmail.com>
2021-11-04 19:21:12 +0200

    change the snake symbol
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/objects.h
+++ include/objects.h
@@ -4,12 +4,12 @@
 #include "coordinate.h"
 
 enum { symbol_target = '+' };
-enum { symbol_snake = '*' };
+enum { symbol_snake = '+' };
 enum { symbol_barrier_plus = 'x' };
 enum { symbol_blank = ' ' };
 
 void 
-show_object_target(const struct coordinate coord);
+show_object_target(const struct coordinate coordinate);
 
 void 
 show_object_snake(const struct coordinate coord);
