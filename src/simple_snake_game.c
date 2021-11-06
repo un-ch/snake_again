de6c6e8e25ab54cb52b5d08d77d84d7474070b1d
Anatolii Charikov <pepsh6881@gmail.com>
2021-11-06 22:59:02 +0200

    a few variable names were changed
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- src/simple_snake_game.c
+++ src/simple_snake_game.c
@@ -33,21 +33,23 @@ int main()
 
 	struct coordinates_deque snake;
 	struct coordinates_list *target, *barrier;
-	struct coordinates coordin;
-	struct round_settings sett;
+	struct coordinates crd;
+	struct round_settings rnd_stt;
 	int direction_signal;
 
 	display_message_center(greeting_message);
-	set_settings_initial_round(&sett);
-	timeout(sett.snake_speed);
+	set_settings_initial_round(&rnd_stt);
+	timeout(rnd_stt.snake_speed);
 	set_objects_another_round(&snake,
 								&target,
 								&barrier,
-								sett,
-								&coordin);
+								rnd_stt,
+								&crd);
 
 	while((direction_signal = getch()) != key_escape) {
-		handle_direction_signal(direction_signal, &coordin, &snake);
+		handle_direction_signal(direction_signal,
+											&crd,
+											&snake);
 		display_target(target);
 		display_barrier(barrier);
 		/*
@@ -57,15 +59,15 @@ int main()
 		if(is_contact_with_borders(snake.first->coord) ||
 			(is_contact_with_barrier(snake.first->coord, barrier))) {
 			display_message_center("crash!");
-			game_settings_decrease(&sett);
-			if(sett.round_num < 1) {
+			game_settings_decrease(&rnd_stt);
+			if(rnd_stt.round_num < 1) {
 				if(affirmative_answer_to_continue_game_request()) {
-					set_settings_initial_round(&sett);
+					set_settings_initial_round(&rnd_stt);
 					set_objects_another_round(&snake,
 												&target,
 												&barrier,
-												sett,
-												&coordin);
+												rnd_stt,
+												&crd);
 					continue;
 				} else {
 					end_program_crash_end();
@@ -74,23 +76,23 @@ int main()
 			set_objects_another_round(&snake,
 										&target,
 										&barrier,
-										sett,
-										&coordin);
+										rnd_stt,
+										&crd);
 		}
 		if(is_contact_with_target(snake.first->coord, target)) {
-			update_after_contact_with_target(&sett,
-											&snake,
-											snake.first->coord);
-			if(sett.current_snake_length > max_snake_length) {
-				game_settings_increase(&sett);
-				if(sett.round_num > max_round_num) {
+			update_after_contact_with_target(&rnd_stt,
+												&snake,
+												snake.first->coord);
+			if(rnd_stt.current_snake_length > max_snake_length) {
+				game_settings_increase(&rnd_stt);
+				if(rnd_stt.round_num > max_round_num) {
 					end_program_win();
 				}
 				set_objects_another_round(&snake,
 											&target,
 											&barrier,
-											sett,
-											&coordin);
+											rnd_stt,
+											&crd);
 			}
 		}
 	}
