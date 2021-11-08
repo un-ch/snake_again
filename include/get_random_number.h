02526a6fa9db7cb835ec1b4e4f4249c21ff7e304
Anatolii Charikov <pepsh6881@gmail.com>
2021-11-08 16:24:45 +0200

    rewrite the get_random_int_number subroutine
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/get_random_number.h
+++ include/get_random_number.h
@@ -2,6 +2,7 @@
 #define GET_RANDOM_NUMBER_H_SENTRY
 
 int
-get_random_int_number(const int max_generated_number);
+get_random_int_number(const int max_generated_number,
+						const int min_generated_number);
 
 #endif
--- src/get_random_number.c
+++ src/get_random_number.c
@@ -3,12 +3,13 @@
 #include "get_random_number.h"
 
 int
-get_random_int_number(const int max_generated_number)
+get_random_int_number(const int min_generated_number,
+						const int max_generated_number)
 {
-	/*
-	const float max_generated_number = 15.0;
-	return (1 + (int)(max_generated_number* rand() / \
-										(RAND_MAX + 1.0)));
-	*/
-	return (rand() % (max_generated_number + 1));
+	int result;
+
+	result = (min_generated_number + (int) \
+		((float)(max_generated_number) * rand() / (RAND_MAX + 1.0)));
+
+	return result;
 }
