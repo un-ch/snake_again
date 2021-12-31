478a74fe2afb2c4a81b4cdf52bc971d9804d7234
Anatolii Charikov <pepsh6881@gmail.com>
2022-01-01 00:35:47 +0200

    greeting_message[] was removed
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/display_message.h
+++ include/display_message.h
@@ -1,8 +1,6 @@
 #ifndef DISPLAY_MESSAGE_H_SENTRY
 #define DISPLAY_MESSAGE_H_SENTRY
 
-static const char greeting_message[] = "Simple Snake Game";
-
 void
 display_message_center(const char *string);
 
