930d047aabfc5409381fda263bb6d3f4012e4f2c
Anatolii Charikov <pepsh6881@gmail.com>
2022-02-09 01:49:26 +0200

    add enumeration all_colors
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/color.h
+++ include/color.h
@@ -3,7 +3,13 @@
 
 enum { border_color_pair = 6 };
 
-int set_pair(int foreground, int background);
+enum all_colors {
+	white_on_black,
+	magenta_on_black,
+	green_on_black,
+	red_on_black,
+	black_on_black
+};
 
 void
 set_border_color();
