4ae7e048fae8547cc671bfb8305a644a1124d9e9
Anatolii Charikov <pepsh6881@gmail.com>
2022-02-09 01:50:50 +0200

    replace set_border_color() with set_color subroutine
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- src/color.c
+++ src/color.c
@@ -3,11 +3,28 @@
 #include "color.h"
 
 void
-set_border_color()
+set_color(int all_colors)
 {
-	int foreground_color = COLOR_WHITE;
-	int background_color = COLOR_BLACK;
-
-	init_pair(border_color_pair, foreground_color, background_color);
-	attrset(COLOR_PAIR(border_color_pair));
+	switch(all_colors) {
+		case white_on_black:
+			init_pair(white_on_black, COLOR_WHITE, COLOR_BLACK);
+			attrset(COLOR_PAIR(white_on_black));
+			break;
+		case magenta_on_black:
+			init_pair(magenta_on_black, COLOR_MAGENTA, COLOR_BLACK);
+			attrset(COLOR_PAIR(magenta_on_black));
+			break;
+		case green_on_black:
+			init_pair(green_on_black, COLOR_GREEN, COLOR_BLACK);
+			attrset(COLOR_PAIR(green_on_black));
+			break;
+		case red_on_black:
+			init_pair(red_on_black, COLOR_RED, COLOR_BLACK);
+			attrset(COLOR_PAIR(red_on_black));
+			break;
+		case black_on_black:
+			init_pair(black_on_black, COLOR_BLACK, COLOR_BLACK);
+			attrset(COLOR_PAIR(black_on_black));
+			break;
+	}
 }
