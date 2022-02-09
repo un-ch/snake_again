3b5d5aa3b0683ec5dd9eb0614c051bec1aeb5bab
Anatolii Charikov <pepsh6881@gmail.com>
2022-02-09 22:59:08 +0200

    add explicit value to avoid using '0' while color pair enumeration
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/color.h
+++ include/color.h
@@ -4,14 +4,13 @@
 enum { border_color_pair = 6 };
 
 enum all_colors {
-	white_on_black,
-	magenta_on_black,
-	green_on_black,
-	red_on_black,
-	yellow_on_black,
-	black_on_black
+	white_on_black		= 1,
+	magenta_on_black	= 2,
+	green_on_black		= 3,
+	red_on_black			= 4,
+	yellow_on_black		= 5,
+	black_on_black		= 6
 };
-
 void
 set_color(int all_colors);
 
