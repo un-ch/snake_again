9cb12a194231e73d110b77c7a9bb1257e1eea62c
Anatolii Charikov <pepsh6881@gmail.com>
2022-02-10 18:48:56 +0200

    change enumeration position
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/color.h
+++ include/color.h
@@ -2,12 +2,12 @@
 #define COLOR_H_SENTRY
 
 enum fgr_bgr_colors {
-	white_on_black		= 1,
+	black_on_black		= 1,
 	magenta_on_black	= 2,
 	green_on_black		= 3,
 	red_on_black			= 4,
 	yellow_on_black		= 5,
-	black_on_black		= 6
+	white_on_black		= 6
 };
 
 void
