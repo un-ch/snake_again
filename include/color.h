a874d677fac37a5c2625c7b53323bc94e49071e0
Anatolii Charikov <pepsh6881@gmail.com>
2022-02-16 21:03:26 +0200

    add a few colors
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- include/color.h
+++ include/color.h
@@ -3,11 +3,14 @@
 
 enum fgr_bgr_colors {
 	black_on_black		= 1,
-	magenta_on_black	= 2,
-	green_on_black		= 3,
-	red_on_black			= 4,
-	yellow_on_black		= 5,
-	white_on_black		= 6
+	white_on_white		= 2,
+	magenta_on_black	= 3,
+	green_on_black		= 4,
+	red_on_black			= 5,
+	yellow_on_black		= 6,
+	white_on_black		= 7,
+	black_on_white		= 8,
+	red_on_white			= 9
 };
 
 void
--- src/color.c
+++ src/color.c
@@ -6,6 +6,14 @@ void
 set_color(enum fgr_bgr_colors colors)
 {
 	switch(colors) {
+		case black_on_black:
+			init_pair(black_on_black, COLOR_BLACK, COLOR_BLACK);
+			attrset(COLOR_PAIR(black_on_black));
+			break;
+		case white_on_white:
+			init_pair(white_on_white, COLOR_WHITE, COLOR_WHITE);
+			attrset(COLOR_PAIR(white_on_white));
+			break;
 		case white_on_black:
 			init_pair(white_on_black, COLOR_WHITE, COLOR_BLACK);
 			attrset(COLOR_PAIR(white_on_black));
@@ -26,9 +34,13 @@ set_color(enum fgr_bgr_colors colors)
 			init_pair(yellow_on_black, COLOR_YELLOW, COLOR_BLACK);
 			attrset(COLOR_PAIR(yellow_on_black));
 			break;
-		case black_on_black:
-			init_pair(black_on_black, COLOR_BLACK, COLOR_BLACK);
-			attrset(COLOR_PAIR(black_on_black));
+		case black_on_white:
+			init_pair(black_on_white, COLOR_BLACK, COLOR_WHITE);
+			attrset(COLOR_PAIR(black_on_white));
+			break;
+		case red_on_white:
+			init_pair(red_on_white, COLOR_RED, COLOR_WHITE);
+			attrset(COLOR_PAIR(red_on_white));
 			break;
 	}
 }
