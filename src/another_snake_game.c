042116a532b3cb0f1e9886cddade739376ea39c6
Anatolii Charikov <pepsh6881@gmail.com>
2024-07-21 14:46:15 +0300

    A few indentations were changed.
    Signed-off-by: Anatolii Charikov <pepsh6881@gmail.com>

--- src/another_snake_game.c
+++ src/another_snake_game.c
@@ -60,26 +60,33 @@ int main()
 			if(round_settngs.round_num < 1) {
 				if(affirmative_answer_to_continue_game_request()) {
 					set_settings_initial_round(&round_settngs);
-					set_objects_another_round(&snake, &target, &barrier,
-																	round_settngs, &coordinate);
+					set_objects_another_round(&snake,
+                                                &target,
+                                                &barrier,
+												round_settngs, &coordinate);
 					continue;
 				} else {
 					end(quit);
 				}
 			}
-			set_objects_another_round(&snake, &target, &barrier,
-															round_settngs, &coordinate);
+			set_objects_another_round(&snake,
+                                        &target,
+                                        &barrier,
+										round_settngs, &coordinate);
 		}
 		if(is_contact_with_target(snake.first->coord, target)) {
-			update_after_contact_with_target(&round_settngs, &snake,
-																					snake.first->coord);
+			update_after_contact_with_target(&round_settngs,
+                                                &snake,
+												snake.first->coord);
 			if(round_settngs.current_snake_length > max_snake_length) {
 				game_settings_increase(&round_settngs);
 				if(round_settngs.round_num > max_round_num) {
 					end(win);
 				}
-				set_objects_another_round(&snake, &target, &barrier,
-																round_settngs, &coordinate);
+				set_objects_another_round(&snake,
+                                            &target,
+                                            &barrier,
+										    round_settngs, &coordinate);
 			}
 		}
 	}
