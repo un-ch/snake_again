4ebff37ec2bd19ad06cc5c6f18ab8177d2e5924c
Anatolii Charikov <acharikov@proton.me>
2025-04-27 00:12:47 +0300

    Format function parameter lists to single line.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/another_snake_game.c
+++ src/another_snake_game.c
@@ -43,11 +43,7 @@ int main()
 
 	set_settings_initial_round(&round_settngs);
 	timeout(round_settngs.snake_speed);
-	set_objects_another_round(&snake,
-                                &target,
-                                &barrier,
-                                round_settngs,
-                                &coordinate);
+	set_objects_another_round(&snake, &target, &barrier, round_settngs, &coordinate);
 
 	while((direction_signal = getch()) != key_escape) {
 		handle_direction_signal(direction_signal, &coordinate, &snake);
@@ -61,34 +57,25 @@ int main()
 			if(round_settngs.round_num < 1) {
 				if(affirmative_answer_to_continue_game_request()) {
 					set_settings_initial_round(&round_settngs);
-					set_objects_another_round(&snake,
-                                                &target,
-                                                &barrier,
-						round_settngs, &coordinate);
+					set_objects_another_round(&snake, &target, &barrier, round_settngs, &coordinate);
 					continue;
 				} else {
 					end(quit);
 				}
 			}
-			set_objects_another_round(&snake,
-                                        &target,
-                                        &barrier,
-					round_settngs, &coordinate);
+			set_objects_another_round(&snake, &target, &barrier, round_settngs, &coordinate);
 		}
 
 		if(is_contact_with_target(snake.first->coord, target)) {
-			update_after_contact_with_target(&round_settngs,
-                                                &snake,
-						snake.first->coord);
+			update_after_contact_with_target(&round_settngs, &snake, snake.first->coord);
+
 			if(round_settngs.current_snake_length > max_snake_length) {
 				game_settings_increase(&round_settngs);
+				
 				if(round_settngs.round_num > max_round_num) {
 					end(win);
 				}
-				set_objects_another_round(&snake,
-                                            &target,
-                                            &barrier,
-					    round_settngs, &coordinate);
+				set_objects_another_round(&snake, &target, &barrier, round_settngs, &coordinate);
 			}
 		}
 	}
