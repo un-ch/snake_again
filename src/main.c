c2deefb27d332764c43d12a87bc9d0ff5e557e7b
Anatolii Charikov <acharikov@proton.me>
2025-04-29 17:09:28 +0300

    Add the display_in_fog_of_war() function calling.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/main.c
+++ src/main.c
@@ -47,8 +47,8 @@ int main()
 
 	while((direction_signal = getch()) != key_escape) {
 		handle_direction_signal(direction_signal, &coordinate, &snake);
-		display_target_in_fog_of_war(snake.first->coord, target);
-		display_barrier_in_fog_of_war(snake.first->coord, barrier);
+
+		display_in_fog_of_war(snake.first->coord, target, barrier);
 
 		if(is_contact_with_borders(snake.first->coord) ||
 		   is_contact_with_barrier(snake.first->coord, barrier)) {
