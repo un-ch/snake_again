58721d3e2f83b540250e8357ae573c9b07bd6458
Anatolii Charikov <acharikov@proton.me>
2025-04-30 11:48:39 +0300

    Remove variable initializations.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/main.c
+++ src/main.c
@@ -34,9 +34,7 @@ int main()
 	curs_set(0);
 
 	struct coordinates_deque snake;
-	snake.first = NULL;
-	snake.last = NULL;
-	struct coordinates_list *target = NULL, *barrier = NULL;
+	struct coordinates_list *target, *barrier;
 	struct coordinates coordinate;
 	struct round_settings round_settngs;
 	int direction_signal;
