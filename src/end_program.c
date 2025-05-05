70c2a467b08827136463c8d137080f918bde7fe6
Anatolii Charikov <acharikov@proton.me>
2025-05-05 17:37:20 +0300

    Add the status local variable. Add malloc_err case.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/end_program.c
+++ src/end_program.c
@@ -7,16 +7,24 @@
 void
 end(enum end_type how)
 {
-	switch(how) {
+	unsigned int status;
+
+	switch (how) {
 		case quit:
 			display_message_center("The end.");
+			status = exit_success_code;
 			break;
 		case win:
-			display_message_center("win!");
+			display_message_center("Win!");
+			status = exit_success_code;
 			break;
+		case malloc_err:
+			fprintf(stderr, "Malloc error.\n");
+			status = how;
 	}
+
 	clear();
 	endwin(); /* restore terminal after ncurses activity: */
 
-	exit(exit_success_code);
+	exit(status);
 }
