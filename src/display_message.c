b584521915b8d80ddabfeb92918d03074032c466
Anatolii Charikov <acharikov@proton.me>
2025-05-05 17:26:14 +0300

    Simplify variable names and unify screen display logic.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/display_message.c
+++ src/display_message.c
@@ -9,31 +9,40 @@
 void
 display_message_center(const char *string)
 {
-	int max_screen_value_y, max_screen_value_x;
-	int string_len;
+	int max_y, max_x;
+	int string_len = string_length(string);
+
+	getmaxyx(stdscr, max_y, max_x);
 
 	clear_screen();
-	getmaxyx(stdscr, max_screen_value_y, max_screen_value_x);
-	string_len = string_length(string);
-	move((max_screen_value_y / 2), (max_screen_value_x - string_len - 1) / 2);
-	set_color(black_on_white);
+
+	move((max_y / 2), (max_x - string_len - 1) / 2);
+	set_color(gray_on_black);
 	addstr(string);
+
 	refresh();
 	sleep(2);
+
+	clear_screen();
 }
 
 void
 display_round_number(const int number)
 {
 	static const char *message = "Round";
-	int max_screen_value_y, max_screen_value_x;
-	int string_len;
+	int max_y, max_x;
+	int string_len = string_length(message);
 
-	getmaxyx(stdscr, max_screen_value_y, max_screen_value_x);
-	string_len = string_length(message);
-	move(max_screen_value_y / 2, (max_screen_value_x - string_len - 1) / 2);
-	set_color(black_on_white);
+	getmaxyx(stdscr, max_y, max_x);
+
+	clear_screen();
+
+	move(max_y / 2, (max_x - string_len - 1) / 2);
+	set_color(gray_on_black);
 	printw("%s %d", message, number);
+
 	refresh();
 	sleep(2);
+
+	clear_screen();
 }
