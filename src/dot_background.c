878bd85c142e31e6107fdb04ab24bc74f42966bb
Anatolii Charikov <acharikov@proton.me>
2025-05-05 17:34:01 +0300

    Shorten local variable names for readability. Add init_dot_background() function implementation.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/dot_background.c
+++ src/dot_background.c
@@ -1,6 +1,6 @@
-#include <ncurses.h>
-
 #include "dot_background.h"
+#include <ncurses.h>
+#include <stdlib.h>
 #include "color.h"
 #include "screen.h"
 #include "objects.h"
@@ -9,19 +9,18 @@
 void
 display_dot_background()
 {
-	int max_screen_height, max_screen_width;
-	/* int min_screen_height, min_screen_width; */
-	int screen_height = 1;
-	int screen_width = 1;
+	int max_y, max_x;
+	int y = 1;
+	int x = 1;
 	struct coordinates crd;
 
-	getmaxyx(stdscr, max_screen_height, max_screen_width);
+	getmaxyx(stdscr, max_y, max_x);
 	set_color(black_on_white);
 
-	for(; screen_height < (max_screen_height - 1); ++screen_height) {
-		for(screen_width = 1; screen_width < (max_screen_width - 1); ++screen_width) {
-			crd.y = screen_height;
-			crd.x = screen_width;
+	for(; y < (max_y - 1); y++) {
+		for(x = 1; x < (max_x - 1); x++) {
+			crd.y = y;
+			crd.x = x;
 			display_object(dot_obj, crd);
 		}
 	}
@@ -31,20 +30,47 @@ void
 display_dot_background_in_fog_of_war(const struct coordinates snake)
 {
 	int x_max, x_min, y_max, y_min;
-	int screen_height, screen_width;
+	int y, x;
+	struct coordinates c;
 
 	x_max = snake.x + 5;
 	x_min = snake.x - 5;
 	y_max = snake.y + 5;
 	y_min = snake.y - 5;
 
-	screen_height = y_min;
-	screen_width = x_min;
+	y = y_min;
+	x = x_min;
 
-	for(; screen_height < (y_max - 1); ++screen_height) {
-		for(screen_width = x_min; screen_width < (x_max - 1); ++screen_width) {
-			move(screen_height, screen_width);
+	for(; y < (y_max - 1); y++) {
+		for(x = x_min; x < (x_max - 1); x++) {
+			move(y, x);
+			c.y = y;
+			c.x = x;
+			display_object(dot_obj, c);
 			addch(symbol_dot);
 		}
 	}
+}
+
+struct coordinates_list *
+init_dot_background(void)
+{
+	struct coordinates_list *first = NULL, *temp;
+	int x, y;
+	int max_height, max_width;
+
+	getmaxyx(stdscr, max_height, max_width);
+
+	for(y = 1; y < (max_height - 1); y++) {
+		for(x = 1; x < (max_width - 1); x++) {
+			temp = malloc(sizeof(struct coordinates_list));
+			/* TODO: malloc error handling; */
+			temp->coord.x = x;
+			temp->coord.y = y;
+			temp->next = first;
+			first = temp;
+		}
+	}
+
+	return first;
 }
