36217023ca8190c02db7b92f4469dcb23279b4c3
Anatolii Charikov <acharikov@proton.me>
2025-05-05 17:37:03 +0300

    Add a few enums.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/end_program.h
+++ src/end_program.h
@@ -1,11 +1,13 @@
 #ifndef END_PROGRAM_H_SENTRY
 #define END_PROGRAM_H_SENTRY
 
-enum { exit_success_code = 0 };
+enum {
+	exit_success_code = 0,
+	exit_failure_code = 1
+};
 
 enum end_type {
-	quit	= 1,
-	win		= 2
+	quit, win, malloc_err
 };
 
 void
