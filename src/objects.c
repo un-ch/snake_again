f5c4289ad0c60359ea8ddef640ef5a1520a287d1
Anatolii Charikov <acharikov@proton.me>
2025-05-05 17:48:22 +0300

    Refactor object display functions: change colors. Add show_object_dot_background() function.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/objects.c
+++ src/objects.c
@@ -3,12 +3,13 @@
 #include "objects.h"
 #include "coordinates.h"
 #include "color.h"
+#include "dot_background.h"
 
 void 
 show_object_target(const struct coordinates crd)
 {
 	move(crd.y, crd.x);
-	set_color(black_on_white);
+	set_color(red_on_black);
 	addch(symbol_target);
 	refresh();
 }
@@ -17,17 +18,26 @@ void
 show_object_snake(const struct coordinates crd)
 {
 	move(crd.y, crd.x);
-	set_color(red_on_white);
+	set_color(green_on_black);
 	addch(symbol_snake);
 	refresh();
 }
 
-void 
+void
 show_object_barrier(const struct coordinates crd)
 {
 	move(crd.y, crd.x);
-	set_color(black_on_white);
+	set_color(gray_on_black);
 	addch(symbol_barrier);
+	refresh();
+}
+
+void
+show_object_dot_background(const struct coordinates crd)
+{
+	move(crd.y, crd.x);
+	set_color(green_on_black);
+	addch(symbol_dot);
 	refresh();
 }
 
@@ -35,7 +45,7 @@ void
 hide_object(const struct coordinates crd)
 {
 	move(crd.y, crd.x);
-	set_color(black_on_white);
+	set_color(gray_on_black);
 	addch(symbol_dot);
 	refresh();
 }
@@ -46,23 +56,23 @@ display_object(enum object obj, const struct coordinates crd)
 	move(crd.y, crd.x);
 	switch(obj) {
 		case target_obj:
-			set_color(black_on_white);
+			set_color(green_on_black);
 			addch(symbol_target);
 			break;
 		case snake_obj:
-			set_color(red_on_white);
+			set_color(red_on_black);
 			addch(symbol_snake);
 			break;
 		case barrier_obj:
-			set_color(black_on_white);
+			set_color(yellow_on_black);
 			addch(symbol_barrier);
 			break;
 		case blank_obj:
-			set_color(black_on_white);
+			set_color(black_on_black);
 			addch(symbol_blank);
 			break;
 		case dot_obj:
-			set_color(black_on_white);
+			set_color(gray_on_black);
 			addch(symbol_dot);
 			break;
 	}
@@ -96,9 +106,16 @@ display_object_in_fog_of_war(const struct coordinates sn,
 
 void
 display_in_fog_of_war(const struct coordinates sn,
-			const struct coordinates_list *t,
-			const struct coordinates_list *b)
+			const struct coordinates_list *tar,
+			const struct coordinates_list *bar,
+			const struct coordinates_list *dot)
 {
-	display_object_in_fog_of_war(sn, b, &show_object_barrier);
-	display_object_in_fog_of_war(sn, t, &show_object_target);
+	/*
+	* Temporary solution to resolve conflicts between targets and barriers:
+	* - Display targets first.
+	* - After displaying targets, barriers will overwrite them.
+	*/
+
+	display_object_in_fog_of_war(sn, tar, &show_object_target);
+	display_object_in_fog_of_war(sn, bar, &show_object_barrier);
 }
--- src/objects.h
+++ src/objects.h
@@ -22,14 +22,16 @@ enum object {
 void show_object_target(const struct coordinates crd);
 void show_object_snake(const struct coordinates crd);
 void show_object_barrier(const struct coordinates crd);
+void show_object_dot_background(const struct coordinates crd);
 void hide_object(const struct coordinates crd);
 void display_object(enum object obj, const struct coordinates crd);
 void display_object_in_fog_of_war(const struct coordinates sn,
 				const struct coordinates_list *list,
 				void (*func)(struct coordinates));
 
-void display_in_fog_of_war(const struct coordinates sn,
-			const struct coordinates_list *t,
-			const struct coordinates_list *b);
-
+void
+display_in_fog_of_war(const struct coordinates sn,
+			const struct coordinates_list *tar,
+			const struct coordinates_list *bar,
+			const struct coordinates_list *dot);
 #endif
