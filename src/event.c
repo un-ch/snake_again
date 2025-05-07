ad2b66bc21ebbfe5fb949d4e036285bb6470ac59
Anatolii Charikov <acharikov@proton.me>
2025-05-07 16:35:12 +0300

    Add event module files.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

---
+++ src/event.c
@@ -0,0 +1,55 @@
+#include "event.h"
+#include "coordinates.h"
+#include "handle_direction.h"
+#include "objects.h"
+#include "borders.h"
+#include "round_settings.h"
+#include "end_program.h"
+#include "barrier.h"
+#include "continue_game_request.h"
+#include "target.h"
+#include "snake_object.h"
+
+void
+handle_event(struct coordinates_deque *snake,
+		struct coordinates_list **tar,
+		struct coordinates_list **bar,
+		struct round_settings *cfg,
+		struct coordinates *crd)
+{
+	display_in_fog_of_war(snake->first->coord, *tar, *bar);
+
+	if (is_contact_with_borders(snake->first->coord) ||
+	    is_contact_with_barrier(snake->first->coord, *bar)) {
+		game_settings_decrease(cfg);
+
+		if (cfg->round_num < 1) {
+			if (affirmative_answer_to_continue_game_request()) {
+				set_settings_initial_round(cfg);
+				set_objects_another_round(snake, tar, bar, *cfg, crd);
+
+				/* TODO: better solution should be done: */
+				return;
+
+			} else {
+				end(quit);
+			}
+		}
+
+		set_objects_another_round(snake, tar, bar, *cfg, crd);
+	}
+
+	if (is_contact_with_target(snake->first->coord, *tar)) {
+		update_after_contact_with_target(cfg, snake, snake->first->coord);
+
+		if (cfg->current_snake_length > max_snake_length) {
+			game_settings_increase(cfg);
+			
+			if (cfg->round_num > max_round_num) {
+				end(win);
+			}
+
+			set_objects_another_round(snake, tar, bar, *cfg, crd);
+		}
+	}
+}
---
+++ src/event.h
@@ -0,0 +1,14 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include "coordinates.h"
+#include "round_settings.h"
+
+void
+handle_event(struct coordinates_deque *snake,
+		struct coordinates_list **tar,
+		struct coordinates_list **bar,
+		struct round_settings *cfg,
+		struct coordinates *crd);
+
+#endif
