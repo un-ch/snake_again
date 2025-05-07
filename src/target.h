8786ddf69b449864fdead1fb3b165467a9201f59
Anatolii Charikov <acharikov@proton.me>
2025-05-07 16:59:05 +0300

    Increase the number of target objects.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/target.h
+++ src/target.h
@@ -1,20 +1,20 @@
-#ifndef TARGET_H_SENTRY
-#define TARGET_H_SENTRY
+#ifndef TARGET_H
+#define TARGET_H
 
 #include "coordinates.h"
 
 #define TRUE	1
 #define FALSE	0
 
-enum { max_target_amount = 30 };
+enum { max_target_amount = 100 };
 
 int
 is_contact_with_target(const struct coordinates crd,
-					struct coordinates_list *target);
+			struct coordinates_list *target);
 
 void
 display_target_in_fog_of_war(struct coordinates snake,
-						struct coordinates_list *target);
+			struct coordinates_list *target);
 
 void
 display_target(struct coordinates_list *target);
