6993d6cfaefe1de0035fa50fe8d02a871a5124f0
Anatolii Charikov <acharikov@proton.me>
2025-05-07 17:20:50 +0300

    Move several lines from main while-loop into handle_event() function.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/main.c
+++ src/main.c
@@ -1,13 +1,7 @@
 #include <time.h>	/* for time() */
 #include <stdlib.h>	/* for srand() */
 #include <ncurses.h>
-
-#include "screen.h"
-#include "string.h"
-#include "display_message.h"
 #include "end_program.h"
-#include "objects.h"
-#include "get_random_number.h"
 #include "coordinates.h"
 #include "barrier.h"
 #include "target.h"
@@ -15,7 +9,7 @@
 #include "round_settings.h"
 #include "handle_direction.h"
 #include "continue_game_request.h"
-#include "borders.h"
+#include "event.h"
 
 int main()
 {
@@ -31,56 +25,18 @@ int main()
 	curs_set(0);
 
 	struct coordinates_deque snake;
-	struct coordinates_list *target, *barrier, *dot_back;
+	struct coordinates_list *target = NULL, *barrier = NULL;
 	struct coordinates coordinate;
-	struct round_settings round_settngs;
+	struct round_settings cfg;
 	int direction_signal;
 
-	set_settings_initial_round(&round_settngs);
-	timeout(round_settngs.snake_speed);
-	set_objects_another_round(&snake, &target,
-				&barrier, &dot_back,
-				round_settngs, &coordinate);
+	set_settings_initial_round(&cfg);
 
-	while((direction_signal = getch()) != key_escape) {
+	set_objects_another_round(&snake, &target, &barrier, cfg, &coordinate);
+
+	while ((direction_signal = getch()) != key_escape) {
 		handle_direction_signal(direction_signal, &coordinate, &snake);
-
-		display_in_fog_of_war(snake.first->coord, target, barrier, dot_back);
-
-		if(is_contact_with_borders(snake.first->coord) ||
-		   is_contact_with_barrier(snake.first->coord, barrier)) {
-			game_settings_decrease(&round_settngs);
-
-			if(round_settngs.round_num < 1) {
-				if(affirmative_answer_to_continue_game_request()) {
-					set_settings_initial_round(&round_settngs);
-					set_objects_another_round(&snake, &target,
-								&barrier, &dot_back,
-								round_settngs, &coordinate);
-					continue;
-				} else {
-					end(quit);
-				}
-			}
-			set_objects_another_round(&snake, &target,
-						&barrier, &dot_back,
-						round_settngs, &coordinate);
-		}
-
-		if(is_contact_with_target(snake.first->coord, target)) {
-			update_after_contact_with_target(&round_settngs, &snake, snake.first->coord);
-
-			if(round_settngs.current_snake_length > max_snake_length) {
-				game_settings_increase(&round_settngs);
-				
-				if(round_settngs.round_num > max_round_num) {
-					end(win);
-				}
-				set_objects_another_round(&snake, &target,
-							&barrier, &dot_back,
-							round_settngs, &coordinate);
-			}
-		}
+		handle_event(&snake, &target, &barrier, &cfg, &coordinate);
 	}
 
 	end(quit);
