8567cff34bcb050714fd8c6c0d8db37e83df2e2d
Anatolii Charikov <acharikov@proton.me>
2025-05-07 16:53:48 +0300

    Fix indentations.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/barrier.c
+++ src/barrier.c
@@ -4,7 +4,7 @@
 
 int
 is_contact_with_barrier(const struct coordinates snake_head,
-						const struct coordinates_list *barrier)
+			const struct coordinates_list *barrier)
 {
 	const struct coordinates_list *temp = barrier;
 	int result = FALSE;
@@ -22,7 +22,7 @@ is_contact_with_barrier(const struct coordinates snake_head,
 
 void
 display_barrier_in_fog_of_war(struct coordinates snake_head,
-							struct coordinates_list *barrier)
+				struct coordinates_list *barrier)
 {
 	struct coordinates_list *temp = barrier;
 	int x_max, x_min, y_max, y_min;
