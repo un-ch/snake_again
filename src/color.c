a884c4ea8bee4294f07311c7668510c214c5bb30
Anatolii Charikov <acharikov@proton.me>
2025-05-12 10:44:08 +0300

    Simplify color selection logic.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/color.c
+++ src/color.c
@@ -1,54 +1,65 @@
-#include <curses.h>
-
 #include "color.h"
+#include <ncurses.h>
+#include "end_program.h"
 
 void
 set_color(enum fgr_bgr_colors colors)
 {
-	switch(colors) {
-		case black_on_black:
-			init_pair(black_on_black, COLOR_BLACK, COLOR_BLACK);
-			attrset(COLOR_PAIR(black_on_black));
-			break;
-		case white_on_white:
-			init_pair(white_on_white, COLOR_WHITE, COLOR_WHITE);
-			attrset(COLOR_PAIR(white_on_white));
-			break;
-		case white_on_black:
-			init_pair(white_on_black, COLOR_WHITE, COLOR_BLACK);
-			attrset(COLOR_PAIR(white_on_black));
-			break;
-		case magenta_on_black:
-			init_pair(magenta_on_black, COLOR_MAGENTA, COLOR_BLACK);
-			attrset(COLOR_PAIR(magenta_on_black));
-			break;
-		case green_on_black:
-			init_pair(green_on_black, COLOR_GREEN, COLOR_BLACK);
-			attrset(COLOR_PAIR(green_on_black));
-			break;
-		case red_on_black:
-			init_pair(red_on_black, COLOR_RED, COLOR_BLACK);
-			attrset(COLOR_PAIR(red_on_black));
-			break;
-		case yellow_on_black:
-			init_pair(yellow_on_black, COLOR_YELLOW, COLOR_BLACK);
-			attrset(COLOR_PAIR(yellow_on_black));
-			break;
-		case black_on_white:
-			init_pair(black_on_white, COLOR_BLACK, COLOR_WHITE);
-			attrset(COLOR_PAIR(black_on_white));
-			break;
-		case red_on_white:
-			init_pair(red_on_white, COLOR_RED, COLOR_WHITE);
-			attrset(COLOR_PAIR(red_on_white));
-			break;
-		case gray_on_black:
-			init_pair(gray_on_black, 0b1000, COLOR_BLACK);
-			attrset(COLOR_PAIR(gray_on_black));
-			break;
-		case cyan_on_black:
-			init_pair(cyan_on_black, COLOR_CYAN, COLOR_BLACK);
-			attrset(COLOR_PAIR(cyan_on_black));
-			break;
+	short fg, bg;
+
+	if (!has_colors()) {
+		end(monochrome_display);
 	}
+
+	switch(colors) {
+	case black_on_black:
+		fg = COLOR_BLACK;
+		bg = COLOR_BLACK;
+		break;
+	case white_on_white:
+		fg = COLOR_WHITE;
+		bg = COLOR_WHITE;
+		break;
+	case white_on_black:
+		fg = COLOR_WHITE;
+		bg = COLOR_BLACK;
+		break;
+	case magenta_on_black:
+		fg = COLOR_MAGENTA;
+		bg = COLOR_BLACK;
+		break;
+	case green_on_black:
+		fg = COLOR_GREEN;
+		bg = COLOR_BLACK;
+		break;
+	case red_on_black:
+		fg = COLOR_RED;
+		bg = COLOR_BLACK;
+		break;
+	case yellow_on_black:
+		fg = COLOR_YELLOW;
+		bg = COLOR_BLACK;
+		break;
+	case black_on_white:
+		fg = COLOR_BLACK;
+		bg = COLOR_WHITE;
+		break;
+	case red_on_white:
+		fg = COLOR_RED;
+		bg = COLOR_WHITE;
+		break;
+	case gray_on_black:
+		fg = color_gray;
+		bg = COLOR_BLACK;
+		break;
+	case cyan_on_black:
+		fg = COLOR_CYAN;
+		bg = COLOR_BLACK;
+		break;
+	default:
+		break;
+	}
+
+	init_pair(colors, fg, bg);
+	attrset(COLOR_PAIR(colors));
 }
