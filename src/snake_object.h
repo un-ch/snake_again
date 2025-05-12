855f45feb95ac0f56d422255da4bdc6f89388293
Anatolii Charikov <acharikov@proton.me>
2025-05-12 11:17:25 +0300

    Place return type on a separate line in function declarations.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/snake_object.h
+++ src/snake_object.h
@@ -8,8 +8,13 @@ enum {
 	max_snake_length = 5
 };
 
-void move_snake_object(struct coordinates_deque *s, struct coordinates d);
-void add_new_snake_element(struct coordinates_deque *s, struct coordinates c);
-void apply_snake_speed(const unsigned int speed);
+void
+move_snake_object(struct coordinates_deque *s, struct coordinates d);
+
+void
+add_new_snake_element(struct coordinates_deque *s, struct coordinates c);
+
+void
+apply_snake_speed(const unsigned int speed);
 
 #endif
