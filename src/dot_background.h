dffd41fc643430652d7955b845a8781e8b2be997
Anatolii Charikov <acharikov@proton.me>
2025-05-17 21:06:07 +0300

    Remove function declarations.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/dot_background.h
+++ src/dot_background.h
@@ -3,12 +3,6 @@
 
 #include "coordinates.h"
 
-void
-display_dot_background(void);
-
-void
-display_dot_background_in_fog_of_war(const struct coordinates snake);
-
 struct coordinates_list *
 init_dot_background(void);
 
