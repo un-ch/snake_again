4ba17059066771da7cfb40c6ad6858dbe3a7146b
Anatolii Charikov <acharikov@proton.me>
2025-05-17 21:00:24 +0300

    Remove function implementations.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- src/dot_background.c
+++ src/dot_background.c
@@ -7,51 +7,6 @@
 #include "coordinates.h"
 #include "end_program.h"
 
-void
-display_dot_background(void)
-{
-	struct coordinates crd;
-	int max_y, max_x;
-	int y, x;
-
-	getmaxyx(stdscr, max_y, max_x);
-	set_color(black_on_white);
-
-	for (y = 1; y < (max_y - 1); y++) {
-		for (x = 1; x < (max_x - 1); x++) {
-			crd.y = y;
-			crd.x = x;
-			display_object(dot_obj, crd);
-		}
-	}
-}
-
-void
-display_dot_background_in_fog_of_war(const struct coordinates snake)
-{
-	int x_max, x_min, y_max, y_min;
-	int y, x;
-	struct coordinates c;
-
-	x_max = snake.x + 5;
-	x_min = snake.x - 5;
-	y_max = snake.y + 5;
-	y_min = snake.y - 5;
-
-	y = y_min;
-	x = x_min;
-
-	for (; y < (y_max - 1); y++) {
-		for (x = x_min; x < (x_max - 1); x++) {
-			move(y, x);
-			c.y = y;
-			c.x = x;
-			display_object(dot_obj, c);
-			addch(symbol_dot);
-		}
-	}
-}
-
 struct coordinates_list *
 init_dot_background(void)
 {
